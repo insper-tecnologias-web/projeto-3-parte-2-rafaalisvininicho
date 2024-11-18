16c831a30bcace0bba2d6ec4efb85a6e4021a2af
rafaela-alexandre <rafaelaoa@al.insper.edu.br>
2024-11-18 17:17:44 -0300

    API

---
+++ lib/api/apis/recipe_api.dart
@@ -0,0 +1,44 @@
+part of '../api_manager.dart';
+
+extension RecipeApi on ApiManager {
+  
+  /// Obtém todas as receitas do backend
+  Future<List<ModelRecipe>> getAllRecipes() async {
+    final response = await get("/recipes/data/");
+    final List<ModelRecipe> recipes = [];
+    for (final item in response) {
+      recipes.add(ModelRecipe.fromJson(item));
+    }
+    return recipes;
+  }
+
+  /// Adiciona uma receita à lista de refeições do usuário
+  Future<void> addRecipe(String recipeId) async {
+    final userId = Hive.box('userData').get('id');
+    await post("/recipes/add/", {
+      'user_id': userId,
+      'recipe_id': recipeId,
+    });
+  }
+
+  /// Marca uma receita como consumida
+  Future<void> markAsConsumed(String recipeId) async {
+    final userId = Hive.box('userData').get('id');
+    await post("/recipes/set/$recipeId/", {
+      'user_id': userId,
+    });
+  }
+
+  /// Obtém todas as refeições do usuário
+  Future<List<ModelRecipe>> getUserMeals() async {
+    final userId = Hive.box('userData').get('id');
+    final response = await post("/recipes/meals/", {
+      'user_id': userId,
+    });
+    final List<ModelRecipe> meals = [];
+    for (final item in response) {
+      meals.add(ModelRecipe.fromJson(item));
+    }
+    return meals;
+  }
+}
--- lib/pages/healthymenu_page.dart
+++ lib/pages/healthymenu_page.dart
@@ -1,13 +1,66 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
-import 'package:projeto/widgets/pad_scaffold.dart';
+class ModelRecipe {
+  final String id;
+  final String title;
+  final String url;
+  final String imageUrl;
+  final String ingredients;
+  final String preparationSteps;
+  final double calorie;
+  final double protein;
+  final double fat;
+  final double carbs;
+  final DateTime consumedAt;
+  final bool wasConsumed;
+  final String mealType;
 
-@RoutePage()
-class HealthyMenuPage extends StatelessWidget {
-  const HealthyMenuPage({super.key});
+  ModelRecipe({
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.imageUrl,
+    required this.ingredients,
+    required this.preparationSteps,
+    required this.calorie,
+    required this.protein,
+    required this.fat,
+    required this.carbs,
+    required this.consumedAt,
+    required this.wasConsumed,
+    required this.mealType,
+  });
 
-  @override
-  Widget build(BuildContext context) {
-    return const PadScaffold(body: Placeholder());
+  factory ModelRecipe.fromJson(Map<String, dynamic> json) {
+    return ModelRecipe(
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+      imageUrl: json['image_url'],
+      ingredients: json['ingredients'],
+      preparationSteps: json['preparation_steps'],
+      calorie: json['calorie'],
+      protein: json['protein'],
+      fat: json['fat'],
+      carbs: json['carbs'],
+      consumedAt: DateTime.parse(json['consumed_at']),
+      wasConsumed: json['was_consumed'],
+      mealType: json['meal_type'],
+    );
+  }
+
+    Map<String, dynamic> toJson() {
+    return{
+      'title': title,
+      'url': url,
+      'image_url': imageUrl,
+      'ingredients': ingredients,
+      'preparation_steps': preparationSteps,
+      'calorie': calorie,
+      'protein': protein,
+      'fat': fat,
+      'carbs': carbs,
+      'consumed_at': consumedAt,
+      'was_consumed': wasConsumed,
+      'meal_type': mealType,
+    };
   }
 }
